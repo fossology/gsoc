eda61bb80602a89be97e8b43da8b4973a04e4177
shaheemazmalmmd <shaheem.azmal@siemens.com>
2025-07-03 04:51:16 +0000

    Deploying to gh-pages from @ fossology/gsoc@1580b899d9c58c0350fb5ece89aff56a130be3ad 🚀

---
+++ assets/js/5e95c892.25ff02e0.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkgosc=self.webpackChunkgosc||[]).push([[9647],{81957:(e,s,r)=>{r.r(s),r.d(s,{default:()=>o});r(96540);var c=r(34164),u=r(7918),a=r(90811),n=r(22831),t=r(79686),l=r(74848);function o(e){return(0,l.jsx)(u.e3,{className:(0,c.A)(a.G.wrapper.docsPages),children:(0,l.jsx)(t.A,{children:(0,n.v)(e.route.routes)})})}}}]);
